53fe0738b83711b30f1b80065e4b793eba5b66b0
Md Jakaria Nur <122019360+mdjakarianurs@users.noreply.github.com>
2024-06-01 18:07:13 +0600

    Create JavaScript use in body.js

---
+++ JavaScript use in body.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h2>Md Jakaria Nur</h2>
+<p id="demo">Software Design and Development Engineer.</p>
+<button type="button" onclick="myFunction()">Try it</button>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "He is also Digital Marketing and Graphics Desgning Engineer.";
+}
+</script>
+</body>
+</html>
