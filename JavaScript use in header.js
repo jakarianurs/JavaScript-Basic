c6d6246aa13f48dcd8858341d03539c8be3b7c57
Md Jakaria Nur <122019360+mdjakarianurs@users.noreply.github.com>
2024-06-01 18:05:09 +0600

    Create JavaScript use in header.js

---
+++ JavaScript use in header.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "He is also Digital Marketing and Graphics Desgning Engineer.";
+}
+</script>
+</head>
+<body>
+<h2>Md Jakaria Nur</h2>
+<p id="demo">Software Design and Development Engineer.</p>
+<button type="button" onclick="myFunction()">Try it</button>
+</body>
+</html>
